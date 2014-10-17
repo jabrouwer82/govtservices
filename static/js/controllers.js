ff150258c9c87f1c94056ef0c99596ed6ebae87e
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-10-17 16:39:54 -0500

    added tracking and switching between tabs

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,5 +1,44 @@
-var app = angular.module('211ServicesApp', [])
+var app = angular.module('211ServicesApp', []);
 
-app.controller('analyticsController', function($scope) {
-    $scope.foo = "bar";
+// Jinja2 & Angular use the same things to designate templing,
+// modify what is used.
+app.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[{');
+  $interpolateProvider.endSymbol('}]}');
+});
+
+// TabsController, controls navigation between different panes
+app.controller('TabsController', function ($scope) {
+
+    // Define the tabs and sections the user can toggle between
+    $scope.tabs = [
+        {
+            title: 'Analytics',
+            icon: 'fa-bar-chart',
+            url: 'analytics.html'
+        },
+        {
+            title: 'Database',
+            icon: 'fa-search',
+            url: 'database.html'
+        },
+        {
+            title: 'Manage Users',
+            icon: 'fa-user',
+            url: 'manage-users.html'
+        }
+    ];
+
+    // Begin on the analytics tab
+    $scope.currentTab = 'analytics.html';
+
+    // When a tab is clicked switch to that tab
+    $scope.onClickTab = function(tab) {
+        $scope.currentTab = tab.url;
+    }
+
+    // The active tab is the current tab
+    $scope.isActiveTab = function(tabUrl) {
+        return tabUrl == $scope.currentTab;
+    }
 });
