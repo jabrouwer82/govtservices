a01b8d14950230f0699542957d23c5649afafe73
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-10-18 00:32:28 -0500

    added sweetalert, began ask watson section (not complete)

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -75,6 +75,20 @@ app.controller('ManageUsersController', function ($scope) {
     $scope.title = 'ManageUsersController';
 })
 
-app.controller('AskWatsonController', function ($scope) {
-    $scope.title = 'AskWatsonController';
+app.controller('AskWatsonController', function ($scope, $http) {
+    $scope.searchQuestion = "";
+    $scope.loading = false;
+
+    $scope.askQuestion = function() {
+        $scope.loading = true;
+
+        $http.get('/ask', {
+            question: 'things things and more things'
+        }).success(function(data, status, headers, config) {
+            scope.loading = false;
+        }).error(function(data, status, headers, config) {
+            // TODO(matthewe|2014-10-17): Handle the error
+            $scope.loading = false;
+        });
+    };
 })
