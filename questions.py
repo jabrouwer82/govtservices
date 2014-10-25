29027f31345ec622d511402f3e3d0e9e84c2d0f7
Jacob Brouwer <jabrouwer82@gmail.com>
2014-10-24 19:51:44 -0500

    Finishes changing questions api to comply w/ mocks
    Fixes bug in previous commit.

--- questions.py
+++ questions.py
@@ -15,24 +15,29 @@ class GetQuestions(utils.Handler):
         query = Question.query()
         query.order(Question.phone_number)
         list_questions = []
+        dict_questions = {}
 
-        def group_by_phone_number(value):
-            return value.phone_number
+        # This is nasty and I like the old itertools way better (commit
+        # 3d48b788384b530b279443bb115e51180f3a9165), but it didn't do
+        # the grouping correctly, so this will have to do until someone
+        # decides to look into what the old way did wrong.
+        for question in query.fetch():
+            entry = dict_questions.get(question.phone_number, {})
+            entry['count'] = entry.get('count', 0) + 1
+            if (not question.time is None
+                  and question.time > entry.get('most_recent', datetime.min)):
+                entry['most_recent'] = question.time
+            dict_questions[question.phone_number] = entry
 
-        for phone_number, questions in itertools.groupby(query.fetch(limit=40),
-                                                 group_by_phone_number):
-            most_recent = datetime.min
-            count = 0
-            for question in questions:
-                count += 1
-                if not question.time is None and question.time > most_recent:
-                    most_recent = question.time
-            
+        for phone_number, entry in dict_questions.iteritems():
+            most_recent = str(entry['most_recent'])
+            count = entry['count']
             list_questions.append({'phone_number': phone_number,
                                    'last_call_date': str(most_recent),
                                    'number_of_questions': count
                                   })
-
+            
+        
         output = json.dumps(list_questions)
         self.render_json(output)
 
@@ -45,11 +50,21 @@ class GetQuestionsForPhoneNumber(utils.Handler):
         phone_number = int(self.request.get('p'))
         query = Question.query()
         query = query.filter(Question.phone_number == phone_number)
-        list_questions = []
-        for question_object in query.fetch(limit=20):
-            list_questions.append({'question': question_object.question,
-                                   'time': str(question_object.time),
-                                   'answer': question_object.answer})
-
-        output = json.dumps(list_questions)
+        
+        questions = []
+        most_recent = datetime.min
+        count = 0
+        
+        for question in query.fetch():
+            count += 1
+            if not question.time is None and question.time > most_recent:
+                most_recent = question.time
+            questions.append({'question': question.question,
+                              'time': str(question.time),
+                              'answer': question.answer})
+        response = {'questions': questions,
+                    'phone_number': phone_number,
+                    'number_of_questions': count,
+                    'most_recent_question': str(most_recent)}
+        output = json.dumps(response)
         self.render_json(output)
